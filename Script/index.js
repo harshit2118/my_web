01c6ce97478539d7052a1cbca1f9de7714cf1080
Harshit Joshi <hrshtjoshi238@gmail.com>
2020-11-30 22:43:33 +0530

    Nav Bar is added and js is applied

---
+++ Script/index.js
@@ -0,0 +1,9 @@
+function goHome(){
+    document.getElementById("middle_scene").scrollIntoView();
+}
+function goAbout(){
+    document.getElementById("profile").scrollIntoView();
+}
+function goSkills(){
+    document.getElementById("middle_scene").scrollIntoView();
+}
