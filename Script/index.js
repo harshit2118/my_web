fd3f596e8a658a449efd685a90752b889efabf5d
Harshit Joshi <hrshtjoshi238@gmail.com>
2020-12-01 17:15:54 +0530

    nav_bar optimized and footer is added

--- Script/index.js
+++ Script/index.js
@@ -1,9 +1,18 @@
 function goHome(){
-    document.getElementById("middle_scene").scrollIntoView();
+    document.getElementById("menu_bar").scrollIntoView();
 }
 function goAbout(){
     document.getElementById("profile").scrollIntoView();
 }
 function goSkills(){
     document.getElementById("middle_scene").scrollIntoView();
+}
+function goLinkeDin(){
+    window.open("https://www.linkedin.com/in/harshit-joshi-5a5782149");
+}
+function goGitHub(){
+    window.open("https://github.com/harshit2118");
+}
+function goFaceBook(){
+    window.open("https://www.facebook.com/harshit.joshi.1004/");
 }
